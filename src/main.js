69292e9c12600f0af5ed4290acd6aed75e7ffca4
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-04-24 15:32:14 -0700

    pass response and convertTo in getElements function. Expanding on else if statements for matching currency values.

--- src/main.js
+++ src/main.js
@@ -8,12 +8,20 @@ import { CurrencyExchange } from "./scripts.js" ;
 $(document).ready(function () {
   $("#exchange").submit(function (event) {
     event.preventDefault();
+    let userInput = parseInt($("#currentDollar").val());
+    let convertTo = $("#convert").val();
 
     (async () => {
-      console.log("made it in the function");
       let userExchange = new CurrencyExchange();
       let response = await userExchange.getLatest();
       console.log(response);
+      getElements();
     })();
-  });
+
+    function getElements(response, convertTo) {
+      if (response === false && convertTo === null) {
+        $("#output").text("There was an error in handling your request. Your currency type may not exist.");
+      } 
+    }
+  }); 
 });
