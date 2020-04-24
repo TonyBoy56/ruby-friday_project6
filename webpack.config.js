b6fce957b502c9b1e7deb3f919a046c30154d9e6
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-04-24 11:28:30 -0700

    add form and button to html.

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ plugins: [
   new CleanWebpackPlugin(),
   new Dotenv(),
   new HtmlWebpackPlugin({
-    title: '',
+    title: 'Currency Exchanger',
     template: './src/index.html',
     inject: 'body'
   })
