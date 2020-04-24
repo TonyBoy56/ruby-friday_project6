2d9e28baecd15554440cbe6dca2c94cea7070701
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-04-24 14:01:00 -0700

    add an async function to pull data from the api. Pull is successful.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 import $ from "jquery";
+// eslint-disable-next-line
 import { CurrencyExchange } from "./scripts.js" ;
 
 $(document).ready(function () {
+  $("#exchange").submit(function (event) {
+    event.preventDefault();
 
+    (async () => {
+      console.log("made it in the function");
+      let userExchange = new CurrencyExchange();
+      let response = await userExchange.getLatest();
+      console.log(response);
+    })();
+  });
 });
