139a214faea2e9715524b2ed7981d8b2a5b5e64c
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-04-24 11:29:02 -0700

    add imports for devDependencies.

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
+import $ from "jquery";
+import { CurrencyExchange } from "./scripts.js" ;
+
+$(document).ready(function () {
+
+});
