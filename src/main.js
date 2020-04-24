8cf0fd4752749ca9583294d1c65ebe28111b774f
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-04-24 16:16:10 -0700

    add else if branching for remaining select options for currency conversions.

--- src/main.js
+++ src/main.js
@@ -19,12 +19,19 @@ $(document).ready(function () {
     })();
 
     function getElements(response, convertTo) {
-      console.log("Hey we made it in");
-      if (response === false && convertTo === null) {
+      if (!response && convertTo === null) {
         console.log("this ain't working, holmes")
         $("#output").text("There was an error in handling your request. Your currency type may not exist.");
-      } else if (response === true && convertTo === "AED") {
-        $("#output").text(`${convertTo}`+ "="+`${response.conversion_rates.AED}`*userInput);
+      } else if (response && convertTo === "AED") {
+        $("#output").text(`${convertTo}`+ " = "+`${response.conversion_rates.AED}`*userInput);
+      } else if (response && convertTo === "BSD") {
+        $("#output").text(`${convertTo}`+ " = "+`${response.conversion_rates.BSD}`*userInput);
+      } else if (response && convertTo === "CAD") {
+        $("#output").text(`${convertTo}`+ " = "+`${response.conversion_rates.CAD}`*userInput);
+      } else if (response && convertTo === "CLP") {
+        $("#output").text(`${convertTo}`+ " = "+`${response.conversion_rates.CLP}`*userInput);
+      } else if (response && convertTo === "COP") {
+        $("#output").text(`${convertTo}`+ " = "+`${response.conversion_rates.COP}`*userInput);
       }
     }
   }); 
