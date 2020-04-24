d0cb4e6a06504773855bca986ce3040553dfc01b
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-04-24 14:01:35 -0700

    remove '?key=' from api call link.

--- src/scripts.js
+++ src/scripts.js
@@ -5,9 +5,11 @@ import './main.js';
 export class CurrencyExchange {
   async getLatest() {
     try {
-      let response = await fetch(`https://prime.exchangerate-api.com/v5/?key=${process.env.API_KEY}/latest/USD`);
+      // eslint-disable-next-line
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
+      let postJSON;
       if (response.status === 200 && response.ok === true) {
-        let postJSON = await response.json();
+        postJSON = await response.json();
       } else {
         postJSON = false;
       }
